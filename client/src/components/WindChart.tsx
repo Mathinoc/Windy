c41a5f49d8e10c802263dcf28e12be12e8977e18
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-07-06 22:06:45 +0200

    added wind direction on graph

--- client/src/components/WindChart.tsx
+++ client/src/components/WindChart.tsx
@@ -1,30 +1,56 @@
-import { Bar } from "react-chartjs-2";
-import { Chart, registerables } from 'chart.js';
+import { Chart } from "react-chartjs-2";
+import { Chart as ChartJS, registerables } from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import formatDate from '../utils/formatDate';
 import colorScale from '../utils/colorScale';
-Chart.register(...registerables);
+import arrow from '../assets/navigation-color.png';
+ChartJS.register(...registerables);
 
 export default function WindChart({ windData }: { windData: { timestamp: string, force: number, dir: number }[] }) {
 
   const graphLabel = windData.map(el => formatDate(el.timestamp));
   const windSpeed = windData.map(el => el.force);
   const barColors = colorScale(windSpeed);
+  const arrowPosition = [-5, -5, -5, -5, -5, -5, -5];
+  const rotation = windData.map(el => el.dir);
+  const imageArrow = new Image();
+  imageArrow.src = arrow;
+
   const data = {
     labels: graphLabel,
-    datasets: [{
-      data: windSpeed,
-      backgroundColor: barColors,
-      borderWidth: 1,
-      hoverBorderWidth: 1.4,
-      datalabels: {
-        anchor: "end" as "end",
-        align: "top" as "top",
-        offset: 10
+    datasets: [
+      {
+        type: "bar" as const,
+        data: windSpeed,
+        backgroundColor: barColors,
+        borderWidth: 1,
+        hoverBorderWidth: 1.4,
+        datalabels: {
+          anchor: "end" as "end",
+          align: "top" as "top",
+          offset: 10,
+        },
       },
-    }]
+      {
+        type: "line" as const,
+        data: arrowPosition,
+        borderColor: "#FFFFFF",
+        hoverBorderWidth: 1.4,
+        pointStyle: imageArrow,
+        rotation: rotation,
+        datalabels: {
+          anchor: "center" as "center",
+          color: 'rgb(0,0,255,0)',
+        },
+      }
+    ]
   }
   const options = {
+    layout: {
+      padding: {
+        bottom: 30,
+      },
+    },
     plugins: {
       title: {
         display: true,
@@ -35,10 +61,39 @@ export default function WindChart({ windData }: { windData: { timestamp: string,
         display: false,
       }
     },
+    scales: {
+
+      yAxes: {
+        barPercentage: 0.9,
+        grid: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: 'Wind speed (knots)',
+          font: {
+            size: 15,
+          }
+        },
+        ticks: {
+          beginAtZero: false,
+          stepSize: 10,
+          min: 0,
+        }
+      },
+      xAxes: {
+        stacked: true,
+        barPercentage: 0.1,
+        grid: {
+          display: false,
+        },
+
+      }
+    }
   }
   return (
     <div>
-      <Bar data={data} plugins={[ChartDataLabels]} options={options} />
+      <Chart data={data} plugins={[ChartDataLabels]} options={options} type={"bar"} />
     </div>
   )
 }
